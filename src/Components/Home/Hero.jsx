1c2f652369234b22ca548b573e3f206e42540d00
Krishnapriya <krishnapriyaprasannan1@gmail.com>
2025-01-16 20:45:35 +0530

    some updates

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -8,40 +8,39 @@ import bharat from "../../assets/bharat.jpeg";
 const Hero = () => {
   return (
     <section className="bg-white">
-           <div className="w-full">
+      <div className="w-full">
         <img
           src={modijiImage}
           alt="Modiji"
-          className="w-full h-auto max-h-[calc(100vh-20px)] sm:max-h-[calc(100vh-60px)] object-cover" 
+          className="w-full h-auto max-h-[calc(100vh-20px)] sm:max-h-[calc(100vh-60px)] object-cover"
         />
       </div>
 
-
       {/* Logo Section */}
-      <div className="bg-white py-6"> {/* Added padding for spacing */}
+      <div className="bg-white py-6">
         <div className="container mx-auto px-4">
           {/* Grid Layout for Logos with responsive design */}
           <div className="grid grid-cols-2 gap-6 sm:grid-cols-4 lg:grid-cols-4">
-            {/* Larger EPR Image */}
+            {/* Logos with adjusted size for better mobile view */}
             <img
               src={bharat}
               alt="Bharat"
-              className="h-20 object-contain sm:h-20 md:h-28 lg:h-32 w-full max-w-[100%]"
+              className="h-10 sm:h-20 md:h-24 lg:h-28 object-contain w-full max-w-[100%]"
             />
             <img
               src={g20}
               alt="G20"
-              className="h-20 object-contain sm:h-20 md:h-28 lg:h-32 w-full max-w-[100%]"
+              className="h-10 sm:h-20 md:h-24 lg:h-28 object-contain w-full max-w-[100%]"
             />
             <img
               src={msme}
               alt="MSME"
-              className="h-20 object-contain sm:h-20 md:h-28 lg:h-32 w-full max-w-[100%]"
+              className="h-10 sm:h-20 md:h-24 lg:h-28 object-contain w-full max-w-[100%]"
             />
             <img
               src={zed}
               alt="ZED"
-              className="h-20 object-contain sm:h-20 md:h-28 lg:h-32 w-full max-w-[100%]"
+              className="h-10 sm:h-20 md:h-24 lg:h-28 object-contain w-full max-w-[100%]"
             />
           </div>
         </div>
