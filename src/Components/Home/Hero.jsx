9de2d15a8dbaad829dc7f76faa82897d5e3fe2dc
Krishnapriya <krishnapriyaprasannan1@gmail.com>
2025-01-16 19:42:56 +0530

    some updates

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -13,35 +13,35 @@ const Hero = () => {
         <img
           src={modijiImage}
           alt="Modiji"
-          className="w-full h-auto max-h-[calc(100vh-50px)] object-cover" // Increased the height a bit
+          className="w-full h-auto max-h-[calc(100vh-60px)] object-cover"
         />
       </div>
 
       {/* Logo Section */}
       <div className="bg-white py-1">
         <div className="container mx-auto px-4">
-          {/* Flex Container for Logos with a smaller height for mobile */}
-          <div className="flex justify-between items-center w-full h-24 sm:h-32 md:h-40 lg:h-48">
+          {/* Flex Container for Logos - Use Grid for Mobile */}
+          <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-4">
             {/* Larger EPR Image */}
             <img
               src={bharat}
               alt="Bharat"
-              className="h-12 sm:h-16 md:h-20 lg:h-24 object-contain w-full max-w-[25%]"
+              className="h-24 object-contain sm:h-32 md:h-40 lg:h-48 w-full max-w-[100%]"
             />
             <img
               src={g20}
               alt="G20"
-              className="h-12 sm:h-16 md:h-20 lg:h-24 object-contain w-full max-w-[25%]"
+              className="h-24 object-contain sm:h-32 md:h-40 lg:h-48 w-full max-w-[100%]"
             />
             <img
               src={msme}
               alt="MSME"
-              className="h-12 sm:h-16 md:h-20 lg:h-24 object-contain w-full max-w-[25%]"
+              className="h-24 object-contain sm:h-32 md:h-40 lg:h-48 w-full max-w-[100%]"
             />
             <img
               src={zed}
               alt="ZED"
-              className="h-12 sm:h-16 md:h-20 lg:h-24 object-contain w-full max-w-[25%]"
+              className="h-24 object-contain sm:h-32 md:h-40 lg:h-48 w-full max-w-[100%]"
             />
           </div>
         </div>
